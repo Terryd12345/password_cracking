63dfaa9eaa5ee05a0dbb81ce62edcb5247fa716c
Terence Denning <terrencedenning@gmail.com>
2019-05-06 17:43:10 +1000

    input converted into hexidecimal strings

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int main(int argc, char *argv){
    unsigned char buffer[320];
@@ -17,14 +18,21 @@ int main(int argc, char *argv){
    }
 
    fread(buffer,sizeof(buffer),1,fp);
- 
- 
+    
+   // Convert binary values into hexidecimal strings 
+   char hex_value[3];
+   char full_hex[32];
+   
+   // Loop through input and create hexidecimal strings 
    for(int i = 0; i<320; i++){
        if( i%32 == 0 ){
-           printf("\n\n");
+           printf("%s\n", full_hex);
+           memset(full_hex, '\0', 32);
        }
-       printf("%02x", buffer[i]);
+       sprintf(hex_value, "%02x", buffer[i]);
+       strncat(full_hex, hex_value, 3);
    }
+   printf("%s", full_hex);
     
    fclose(fp);
    printf("\n\n");
